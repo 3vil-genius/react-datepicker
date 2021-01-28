8af6d4ab676e44325ea382e869051283ac6338c3
Tony Dong <neverlosty@gmail.com>
2021-01-28 11:05:31 +0000

    tweaking release so it works on our repo

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,8 @@ npm version $1
 
 npx react-docgen ./src/*.jsx | node ./scripts/buildDocs.js
 
+yarn run build
+
 git add .
 
 git commit -m "Publish new API docs (automated commit)"
@@ -18,8 +20,3 @@ git commit -m "Publish new API docs (automated commit)"
 git push
 
 git push --tags
-
-yarn run build
-
-npm publish
-
